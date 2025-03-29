e5a0f429046ddb205e13b55d848b7b5985c0f542
EmanoelCariolando <emanoelcariolando@gmail.com>
2025-03-28 21:37:43 -0300

    vote commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Votes } from '@/components/voting';
+
+const Page = () => {
+    return (
+        <>
+            <Votes rate={5}/>
+            
+
+        </>
+    );
+}
+
+export default Page;
