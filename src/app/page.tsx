98b61c4743d7eb564b45f75b649fe2a3bdf206c7
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-04-03 13:11:34 -0300

    new click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 "use client";
-import { BaseStude } from "@/components/base";
-import { Students } from "@/data/people";
+import { FormEvent, useState } from "react";
+
 const Page = () => {
+  const [value, setCount] = useState(false);
+
+  const handleClick = () => {
+    setCount(!value);
+  }
+
+  
+
     return (
-     <div className="container mx-auto">
-
-      <BaseStude people = {Students}/>
-
+      <div className="w-screen h-screen flex items-center justify-center flex-col ">
+        <div>
+          {value &&
+          <div className="bg-green-500 p-2 rounded-md">Secret Area</div>
+       
+           }
+        </div>
+        <button onClick={handleClick} className="bg-blue-500 p-2 rounded-md m-2">{value ? 'ocultar' : 'Show'}</button>
      </div>
     );
 }
