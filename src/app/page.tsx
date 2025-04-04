4e31fe3e9e5967943c4cbac01655c82844482dd5
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-04-04 19:06:30 -0300

    Set valor uptade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,21 @@
 import { FormEvent, useState } from "react";
 
 const Page = () => {
-  const [value, setCount] = useState(false);
-
-  const handleClick = () => {
-    setCount(!value);
+  const [valor, setValor] = useState(0);
+  const click = () => {
+    setValor(valor + 2);
+    setValor(c => c + 2);
   }
-
   
+ 
 
     return (
       <div className="w-screen h-screen flex items-center justify-center flex-col ">
-        <div>
-          {value &&
-          <div className="bg-green-500 p-2 rounded-md">Secret Area</div>
-       
-           }
-        </div>
-        <button onClick={handleClick} className="bg-blue-500 p-2 rounded-md m-2">{value ? 'ocultar' : 'Show'}</button>
+        <div>{valor}</div>
+        <div onClick={click} className="p-2 bg-blue-500 text-white">+2</div>
+
      </div>
+
     );
 }
 
