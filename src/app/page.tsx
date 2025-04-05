41444eec4e5740f0396cbc9345e1964912f0a6ad
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-04-05 16:03:14 -0300

    Uptade Complet Name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
 "use client";
-import { FormEvent, useState } from "react";
-
-const Page = () => {
-  const [valor, setValor] = useState(0);
-  const click = () => {
-    setValor(valor + 2);
-    setValor(c => c + 2);
-  }
-  
+import { useState } from "react";
+import {firstName} from "@/types/typeg";
+import {lastName} from "@/types/typeg";
  
+const Page = () => {
+    const [count, setCount] = useState("");
+    const [countWo, setCountWo] = useState("");
 
     return (
       <div className="w-screen h-screen flex items-center justify-center flex-col ">
-        <div>{valor}</div>
-        <div onClick={click} className="p-2 bg-blue-500 text-white">+2</div>
+
+       <input type="text" placeholder="Name" 
+        onChange={e => setCount (e.target.value)}
+        className="bg-gray-400 m-2 rounded-md text-black p-2"
+       />
+     
+       <input type="text"
+        placeholder="Last Name" 
+        className="bg-gray-400 m-2 rounded-md text-black p-2"
+        onChange={e => setCountWo (e.target.value)}
+       />
+
+       <p className="flex justify-center items-center flex-col"> Your Name: <p className="text-blue-400">{`${count} ${countWo}`}</p> </p>
 
      </div>
 
--- src/types/typeg.ts
+++ src/types/typeg.ts
@@ -1,9 +1,7 @@
-export type AllStudents = {
-    id : number,
-    name: string,
-    active: boolean,
-    email:string,
-    avatar: string,
-    grade: number,
+export type firstName = {
+    name: string;
+}
+export type lastName = {
+    lastName: string;
 }
   
