2d86da09e11cb322f0f24cc74a13938d5cfc6a0a
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-04-29 19:13:04 -0300

    uso do useEffeect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,20 @@
 "use client";
 
-import { VideoPlayer } from "@/components/base";
-import { img } from "framer-motion/client";
+
+import { Square } from "@/components/base";
 import { useState } from "react";
 
 
 const Page = () => {
-  const [playing, setPlaying] = useState(false)
+  const [show,setShow] = useState(false)
 
 
   return (
      <div className="w-screen h-screen flex flex-col text-center justify-center items-center bg-white">
       <div> 
-      <VideoPlayer
- 
-        src="https://www.w3schools.com/html/movie.mp4"
-        isPlaying = {playing}
-        />
-        <button onClick={() => setPlaying(!playing)} className="bg-white text-black p-2">{playing ?<img className="w-10 h-10" src="../Assents/pausee.png"></img>  : <img className="w-10 h-10" src="../Assents/play.png"></img> }</button>
+        <button onClick={() => setShow(!show)} className="w-20 h-10 bg-white text-black font-bold p-2 border border-black">Show</button>
+
+        {show && <Square/>}
         
       </div>
      </div>
--- src/components/base.tsx
+++ src/components/base.tsx
@@ -1,18 +1,15 @@
-import { div } from "framer-motion/client"
-
-
-
-
+import { useEffect, useState } from "react"
 
  export const Square = ( )=> {
+
+    useEffect(() => {
+      console.log('CONECTOU')
+
+      return () => {
+        console.log('DESCONECTED')}
+      },[]);
     
-
   return (
- 
-   <div className="w-30 h-30 bg-amber-600 ">
-     
-   </div>
-      
+   <div className="w-30 h-30 bg-amber-600 m-2 font-bold text-white ">1</div>
   )
-
  }
