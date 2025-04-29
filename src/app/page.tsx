1007693c354fb0d18dc70caab1f5b1e2113dfc58
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-04-29 11:16:47 -0300

    Video Pause and Play

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,27 @@
 "use client";
 
+import { VideoPlayer } from "@/components/base";
+import { img } from "framer-motion/client";
 import { useState } from "react";
-import { allQuestions } from "@/data/people";
-import { QuestionItem } from "@/components/base";
-import { Resultados } from "@/components/resulta";
 
 
 const Page = () => {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [answersA, setAnswers] = useState<number[]>([])
-  const [results, showResults] = useState(false)
+  const [playing, setPlaying] = useState(false)
 
-  const loadNextQuestion = () => {
-   if(allQuestions[currentQuestion + 1])
-    setCurrentQuestion(currentQuestion + 1)
-  else {
-    showResults(true)
-  }
-  }
-
-
-  const handle = (answer: number) => {
-    setAnswers([...answersA, answer]);
-    loadNextQuestion()
-  }
-  
-  const title = "🎮 Gamer Quiz"
 
   return (
-     <div className=" w-full h-screen flex items-center justify-center bg-gray-600">
-        <div className="bg-white text-black rounded-md shadow shadow-black w-full max-w-xl">
-          <h1 className="m-2 text-2xl font-bold">{title}</h1>
-  
-          <div className="text-center p-4 border border-t border-gray-800 rounded-md m-2 hover:text-black">
-            {!results &&
-            <QuestionItem 
-              question={allQuestions[currentQuestion]}
-              count={currentQuestion + 1}
-              onAnswer={handle}
-            />
-            }
-            { results &&
-              <Resultados questions={allQuestions} answers={answersA} />
-            }
-          </div>
-          <div className="flex justify-center items-center">
-          { !results &&
-             <div className="text-center text-xs mt-5 mb-2 font-bold"> {currentQuestion + 1} questions de {allQuestions.length}</div>
-          }  
-          { results &&
-             <div className="text-white bg-blue-600 rounded-2xl p-2 m-5 font-bold text-center cursor-pointer ">Restart</div>
-          }  
-          </div>
-        </div>
-    </div>
+     <div className="w-screen h-screen flex flex-col text-center justify-center items-center bg-white">
+      <div> 
+      <VideoPlayer
+ 
+        src="https://www.w3schools.com/html/movie.mp4"
+        isPlaying = {playing}
+        />
+        <button onClick={() => setPlaying(!playing)} className="bg-white text-black p-2">{playing ?<img className="w-10 h-10" src="../Assents/pausee.png"></img>  : <img className="w-10 h-10" src="../Assents/play.png"></img> }</button>
+        
+      </div>
+     </div>
+
   );
 };
 
--- src/components/base.tsx
+++ src/components/base.tsx
@@ -1,51 +1,29 @@
-import { Questions } from "@/types/typeg";
-import { useState } from "react";
+import { useEffect, useRef } from "react";
+
 
  type Props = {
-  question: Questions;
-  count: number;
-  onAnswer: (answer: number) => void; //onAnswer(0)
+  src: string;
+  isPlaying: boolean;
  } 
 
- export const QuestionItem = ({question,count,onAnswer}: Props) => {
-  const [selectedAnswer, setSelectedAnswer] = useState< number | null >(null); //selectedAnswer(null)
+ export const VideoPlayer = ( {src,isPlaying}: Props )=> {
+    const videoTag = useRef<HTMLVideoElement>(null);
 
-   const checkQuestion = (key: number) => { 
-    if (selectedAnswer === null) {
-      setSelectedAnswer(key);
 
-      setTimeout((  ) => {
-        setSelectedAnswer(null);
-        onAnswer(key);
-      }, 400)
-    }
-     
-   }
+    useEffect(() => {
+        if(isPlaying) {
+            videoTag.current?.play()
+        } else {
+            videoTag.current?.pause()
+        }
+    })
+
+
   return (
-     <div>
-        <h1 className="font-bold ">{count}. {question.question}</h1>
-        <div>
-          {question.options.map((i, respo) => (
+ 
+      <video ref={videoTag} src={src} loop playsInline></video>
+      
 
-            <div
-              key={respo}
-              onClick={() => {checkQuestion(respo)}}
-              className={`text-center p-2 border border-gray-400 rounded-md m-2 hover:text-black
-              ${selectedAnswer !== null ? "cursor-auto hover:opacity-100" : " cursor-pointer hover:opacity-60 "}
-              ${selectedAnswer !== null &&  selectedAnswer === question.aswer && selectedAnswer === respo && "  border-2 border-green-600 bg-green-100"}
-              ${selectedAnswer !== null &&  selectedAnswer !== question.aswer && selectedAnswer === respo && " bg-red-500 "}
-  
-
-              `}>
-                {i}</div>
-
-              
-
-
-          ))}
-        </div>
-
-     </div>
   )
- }
 
+ }
